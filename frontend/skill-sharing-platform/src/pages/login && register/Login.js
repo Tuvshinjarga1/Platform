99545e40a9e18c085da8be0412281cdc05f31d2b
Tuvshinjarga1 <65581854+Tuvshinjarga1@users.noreply.github.com>
2025-02-26 17:44:42 +0800

    Update Login.js

--- frontend/skill-sharing-platform/src/pages/login && register/Login.js
+++ frontend/skill-sharing-platform/src/pages/login && register/Login.js
@@ -23,7 +23,7 @@ export function Login() {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await axios.post('http://localhost:5000/api/login', {
+      const response = await axios.post('https://platform-backend-zxgy.onrender.com/api/login', {
         email,
         password,
       });
