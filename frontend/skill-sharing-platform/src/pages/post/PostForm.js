22d8a340597831b70e33306c7697d7dc5106b0ff
Tuvshinjarga1 <65581854+Tuvshinjarga1@users.noreply.github.com>
2025-02-26 17:45:43 +0800

    Update PostForm.js

--- frontend/skill-sharing-platform/src/pages/post/PostForm.js
+++ frontend/skill-sharing-platform/src/pages/post/PostForm.js
@@ -65,7 +65,7 @@ const PostForm = () => {
 
     try {
       const postData = { title, description, category, readingTime };
-      const response = await axios.post("http://localhost:5000/api/posts", postData, {
+      const response = await axios.post("https://platform-backend-zxgy.onrender.com/api/posts", postData, {
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
